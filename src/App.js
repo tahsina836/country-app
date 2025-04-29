ccfc0bc90bbd8755e6f4de2288cc749cf45a16c4
tahsina836 <tahsinaafrin836@gmail.com>
2025-04-29 10:56:08 +0600

    3rd commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import { useState,useEffect } from 'react';
 import Countries from './component/Countries';
 import './App.css';
-
-
-const url="https://restcountries.com/v3.1/all" ;  //prothome link ta declear krte hbe 
-
+import Search from './component/Search';
+const url="https://restcountries.com/v3.1/all" ;//prothome link ta declear krte hbe 
 const App = () => {
-const[isLoading,setIsLoading]=useState(true);  //then ei 3ta useState declear cz load,err egla link erupor depend krei hbe
+const[isLoading,setIsLoading]=useState(true); //then ei 3ta useState declear cz load,err egla link erupor depend krei hbe
 const[error,setError]=useState(null);        //means age amra url nibo then oitar state decide krbo                  
 const[countries,setCountries]=useState([]);
 const[filteredCountries,setFilteredCountries]=useState([countries]);
-
 const fetchData = async(url)=>{   
-  setIsLoading(true);     //eta function jeta niche call kora hoise useEffect er vitore
-try{
-  const response=await fetch(url);           //url ta k fetch kre response e rakhbo
-  const data = await response.json();         // response k json e convert kre data te rakhbo
+  setIsLoading(true);//eta function jeta niche call kora hoise useEffect er vitore
+try{const response=await fetch(url);//url ta k fetch kre response e rakhbo
+  const data = await response.json(); // response k json e convert kre data te rakhbo
 setCountries(data);  
-setFilteredCountries(data);             // setcountries a data set kre dibo taile shb countries data te chole ashbe 
+setFilteredCountries(data);// setcountries a data set kre dibo taile shb countries data te chole ashbe 
 setIsLoading(false);
 setError(null);
 }
@@ -26,21 +22,28 @@ catch(error){
   setIsLoading(false);
 setError(error);
 }
-console.log(countries);
-};
-
+console.log(countries);};
 useEffect(()=>{
-  fetchData (url);
-},[]);
-
+  fetchData (url);},[]);
 const handleRemoveCountry=(name)=>{
 const filter=filteredCountries.filter((country)=>country.name.common !==name);setFilteredCountries(filter)
   };
+  const handleSearch=(searchValue)=>{
+ let value=searchValue.toLowerCase();
+ const newCountries=countries.filter((country)=>{
+  const countryName=country.name.common.toLowerCase;
+  return countryName.startsWith(value)
+ });
+ setFilteredCountries(newCountries);
+  };
     return <>
     <h2>Country App</h2>
+     <Search onSearch={handleSearch}/>
     {isLoading && <p> . . . is Loading </p>}
     {error && <h2>{error.message}</h2> }
-    {countries && (<Countries countries={filteredCountries} onRemoveCountry={handleRemoveCountry}/>)}
+    {
+    countries && (<Countries countries={filteredCountries} onRemoveCountry={handleRemoveCountry}/>)
+    }
     </>
   };  
 export default App;
